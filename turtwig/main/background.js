aafc3072bd75d7cdd20270d3dadac980cff4a4dc
Maria Korolik <40607581+mkorolik@users.noreply.github.com>
2026-05-28 17:19:24 +0200

    reorganizing for updates

---
+++ turtwig/main/background.js
@@ -0,0 +1,13 @@
+function checkForReddit() {
+  chrome.tabs.query({}, (tabs) => {
+    const redditOpen = tabs.some(tab => tab.url && tab.url.includes("reddit.com"));
+
+    chrome.action.setPopup({
+      popup: redditOpen ? "sad.html" : "hello.html"
+    });
+  });
+}
+
+chrome.tabs.onUpdated.addListener(checkForReddit);
+chrome.tabs.onRemoved.addListener(checkForReddit);
+chrome.tabs.onCreated.addListener(checkForReddit);
